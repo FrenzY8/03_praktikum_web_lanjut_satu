e4c225c1628c6dee1a97b8643a7657a86bdcf11f
pren <75056113+FrenzY8@users.noreply.github.com>
2026-01-26 07:17:39 +0700

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,8 +10,49 @@ class PageController extends Controller
     {
         return view('home');
     }
+
     public function about()
     {
         return view('about');
+    }
+
+    public function service()
+    {
+        return view('service');
+    }
+
+    public function team()
+    {
+        return view('team');
+    }
+
+    public function price()
+    {
+        return view('price');
+    }
+
+    public function blog()
+    {
+        return view('blog');
+    }
+
+    public function detail()
+    {
+        return view('detail');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    public function appointment()
+    {
+        return view('appointment');
+    }
+
+    public function search()
+    {
+        return view('search');
     }
 }
